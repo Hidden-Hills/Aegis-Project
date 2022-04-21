edf9c7d0aaed7e0af565b93625b41e667e367b51
Hidden-Hills <74623571+Hidden-Hills@users.noreply.github.com>
2022-04-21 05:32:34 -0400

    Delete AegisAPI directory

--- AegisAPI/myapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import CrimeDetectionData, Hero, LostInventoryData, Notification
-
-admin.site.register(Hero)
-admin.site.register(Notification)
-admin.site.register(LostInventoryData)
-admin.site.register(CrimeDetectionData)
